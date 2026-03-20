d22146f0896a7e69ceded09cd424f28c5eb9ef56
sunmyeonglee <03leesun@gmail.com>
2026-03-20 22:53:27 +0900

    feat: Add Google Analytics (GA4) gtag.js

--- app/layout.tsx
+++ app/layout.tsx
@@ -74,6 +74,12 @@ export default function RootLayout({
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-T3QGXJZL');`,
           }}
         />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-8L3LM6Q57N" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-8L3LM6Q57N');`,
+          }}
+        />
       </head>
       <body suppressHydrationWarning style={{ opacity: 0 }}>
         <noscript>
